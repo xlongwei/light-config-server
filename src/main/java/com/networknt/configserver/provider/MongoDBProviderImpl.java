b3d4c1735cad191510926e46579e38feb1b7cba9
Balloon <whenballoon@gmail.com>
2020-11-18 09:20:44 -0500

    - support mongodb - added mongodb provider

---
+++ src/main/java/com/networknt/configserver/provider/MongoDBProviderImpl.java
@@ -0,0 +1,158 @@
+package com.networknt.configserver.provider;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.networknt.config.Config;
+import com.networknt.configserver.constants.ConfigServerConstants;
+import com.networknt.configserver.model.Service;
+import com.networknt.configserver.model.ServiceConfigs;
+import com.networknt.exception.ApiException;
+import org.bson.Document;
+
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MongoDBProviderImpl implements IProvider {
+  private static final String CONFIGS_FILE_NAME = "values";
+  private static final String MONGO_DB_URI = "mongoDBUri";
+  private static final String MONGO_DB_NAME = "mongoDBName";
+  private static final String MONGO_COLLECTION_NAME = "configs";
+
+  //init mongodb connection
+  Map<String, Object> configServerConfig = Config.getInstance().getJsonMapConfig(ConfigServerConstants.CONFIG_NAME);
+  String url = (String) configServerConfig.get(MONGO_DB_URI);
+  String dbName = (String) configServerConfig.get(MONGO_DB_NAME);
+  String collectionName = (String) configServerConfig.get(MONGO_COLLECTION_NAME);
+  MongoClient mongoClient = MongoClients.create(System.getProperty(url));
+  MongoDatabase database = mongoClient.getDatabase(dbName);
+  MongoCollection<Document> collection = database.getCollection(collectionName);
+
+  @Override
+  public String login(String authorization) throws ApiException {
+    return null;
+  }
+
+  /**
+   * Get config properties from config directory for given service details
+   *
+   * @param authToken will be used for validate service id
+   * @param service:  object with service details like projectName, projectVersion, serviceName, serviceVersion etc. to create config directory configs path
+   * @return serviceConfigs
+   * @throws ApiException when fails to fetch configs from config directory
+   */
+  @Override
+  public ServiceConfigs getServiceConfigs(String authToken, Service service) throws ApiException {
+    ServiceConfigs serviceConfigs = new ServiceConfigs();
+    serviceConfigs.setConfigProperties(new HashMap<String, Object>());
+    serviceConfigs.setService(service);
+    String configPath = null;
+    Map<String, Object> configsMap = null;
+
+    //Get Global configs
+    configPath = buildConfigKey(service, ConfigServerConstants.CONFIGS, ConfigServerConstants.GLOBALS, service.getProjectVersion());
+    configsMap = Config.getInstance().getJsonMapConfigNoCache(CONFIGS_FILE_NAME, configPath);
+    if (configsMap != null) {
+      ((Map<String, Object>) serviceConfigs.getConfigProperties()).putAll(configsMap);
+    }
+    //Get Service configs
+    configPath = buildConfigKey(service, ConfigServerConstants.CONFIGS, service.getServiceName(), service.getServiceVersion());
+    configsMap = Config.getInstance().getJsonMapConfigNoCache(CONFIGS_FILE_NAME, configPath);
+    if (configsMap != null) {
+      ((Map<String, Object>) serviceConfigs.getConfigProperties()).putAll(configsMap);
+    }
+    return serviceConfigs;
+  }
+
+  /**
+   * Get certs from mongoDB for given service details
+   *
+   * @param authToken will be used for validate service id
+   * @param service:  object with service details like projectName, projectVersion, serviceName, serviceVersion etc. to create config directory certs path
+   * @return service certs
+   * @throws ApiException when fails to fetch certs from config directory
+   */
+  @Override
+  public ServiceConfigs getServiceCertificates(String authToken, Service service) throws ApiException {
+    return getServiceConfigs(service, ConfigServerConstants.CERTS);
+  }
+
+  /**
+   * Get files from config directory for given service details
+   *
+   * @param authToken will be used for validate service id
+   * @param service:  object with service details like projectName, projectVersion, serviceName, serviceVersion etc. to create config directory files path
+   * @return service files
+   * @throws ApiException when fails to fetch files from config directory
+   */
+  @Override
+  public ServiceConfigs getServiceFiles(String authToken, Service service) throws ApiException {
+    return getServiceConfigs(service, ConfigServerConstants.FILES);
+  }
+
+  private ServiceConfigs getServiceConfigs(Service service, String files) {
+    ServiceConfigs serviceConfigs = new ServiceConfigs();
+    serviceConfigs.setService(service);
+    serviceConfigs.setConfigProperties(new HashMap<String, Object>());
+    String configKey = null;
+    Map<String, Object> configMap = null;
+
+    //Get Global files
+    configKey = buildConfigKey(service, files, ConfigServerConstants.GLOBALS, service.getProjectVersion());
+    configMap = getConfigs(configKey);
+    if (configMap != null) {
+      ((Map<String, Object>) serviceConfigs.getConfigProperties()).putAll(configMap);
+    }
+    //Get Service files
+    configKey = buildConfigKey(service, files, service.getServiceName(), service.getServiceVersion());
+    configMap = getConfigs(configKey);
+    if (configMap != null) {
+      ((Map<String, Object>) serviceConfigs.getConfigProperties()).putAll(configMap);
+    }
+    return serviceConfigs;
+  }
+
+  @Override
+  public List<Service> searchServices(String authToken, String projectName) throws ApiException {
+    return null;
+  }
+
+  private String buildConfigKey(Service service, String configType, String name, String version) {
+    StringBuffer configPath = new StringBuffer();
+    configPath.append(ConfigServerConstants.DASH).append(configType)
+            .append(ConfigServerConstants.DASH).append(service.getProjectName())
+            .append(ConfigServerConstants.DASH).append(name)
+            .append(ConfigServerConstants.DASH).append(version)
+            .append(ConfigServerConstants.DASH).append(service.getEnvironment());
+    return configPath.toString();
+  }
+
+  private Map<String, Object> getConfigs(String configKey) {
+    Map<String, Object> configsMap = new HashMap<>();
+
+    Document document = new Document("_id", configKey);
+    Document entry;
+    try {
+      entry = collection.find(document).first();
+      if (entry == null || entry.isEmpty()) {
+        logger.error("cannot get entry from mongo db with key: {}", configKey);
+        return configsMap;
+      }
+      logger.debug("loaded config with content {}", entry);
+      List<Document> configs = ((List)entry.get("configs"));
+      configs.forEach(config -> {
+        byte[] content = config.getString("content").getBytes();
+        String encodedContent = Base64.getMimeEncoder().encodeToString(content);
+        configsMap.put(config.getString("configName"), encodedContent);
+      });
+
+    } catch (Exception e) {
+      logger.error("cannot get entry from mongo db with key: {}", configKey);
+      return configsMap;
+    }
+    return configsMap;
+  }
+}
---
+++ src/test/java/com/networknt/configserver/provider/MongoDBProviderImplTest.java
@@ -0,0 +1,56 @@
+package com.networknt.configserver.provider;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class  MongoDBProviderImplTest {
+  String configKey = "files/eaap/0.0.1/eaap-service/0.0.1/dev";
+  MongoClient mongoClient = MongoClients.create("mongodb://root:example@0.0.0.0:27017");
+  MongoDatabase database = mongoClient.getDatabase("configServer");
+  MongoCollection<Document> collection = database.getCollection("configs");
+
+  @Test
+  public void testInsert() throws IOException {
+
+    Document insert = new Document("_id", configKey);
+    List<Document> collect = Files.list(FileSystems.getDefault().getPath("src/test/resources/config")).map(path -> {
+      try {
+        String content = Files.readAllLines(path).stream().collect(Collectors.joining());
+        return new Document("configName", path.getFileName().toString()).append("content", content);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return null;
+    }).filter(Objects::nonNull).collect(Collectors.toList());
+    insert.append("configs", collect);
+    collection.insertOne(insert);
+  }
+
+  @Test
+  public void testFind() {
+    String configKey = "files/eaap/0.0.1/eaap-service/0.0.1/dev";
+    Map<String, Object> configsMap = new HashMap<>();
+    Document document = new Document("_id", configKey);
+    Document entry;
+    entry = collection.find(document).first();
+    if (entry != null && !entry.isEmpty()) {
+      List<Document> configs = ((List)entry.get("configs"));
+      configs.forEach(config -> {
+        byte[] content = config.getString("content").getBytes();
+        String encodedContent = Base64.getMimeEncoder().encodeToString(content);
+        configsMap.put(config.getString("configName"), encodedContent);
+      });
+    }
+  }
+}
